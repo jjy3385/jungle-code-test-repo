7ffdfe9a85b5ae05479ecab6d5c54767848dd068
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-11 13:42:29 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,10 +1,7 @@
-C = int(input())
-for _ in range(C):
+for _ in range(int(input())):
     N,*scores = map(int,input().split())
-    sum,cnt = 0,0
-    for s in scores:
-        sum += s
-    avg = sum / N
+    avg = sum(scores) / N
+    cnt = 0
     for s in scores:
         if s > avg:
             cnt += 1
