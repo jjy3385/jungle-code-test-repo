1b4c1c283a32ed9c0827497d9315815eb2c1e04c
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-17 01:35:09 +0900

    [Silver I] Title: 안전 영역, Time: 1720 ms, Memory: 35240 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -4,36 +4,31 @@ sys.setrecursionlimit(10**6)
 N = int(input())
 W = [list(map(int,input().split())) for _ in range(N)]
 
-def dfs(i,j,rain,visited):
-    # base case?
-    # 1.범위를 벗어난 경우, 왜 여기 안걸리는게 나오는건지 이해가 안가
-    # print(f"좌표({i},{j})")
-    if i < 0 or i >= N or j < 0 or j >= N:
-        return
-    # 2. 이미 방문했거나 잠긴경우
-    if visited[i][j] or W[i][j] <= rain:
-        return 
-
-    visited[i][j] = True
-    # 오른쪽 
-    dfs(i,j+1,rain,visited)
-    # 왼쪽
-    dfs(i,j-1,rain,visited)    
-    # 위
-    dfs(i-1,j,rain,visited)    
-    # 아래
-    dfs(i+1,j,rain,visited)    
+# 상우하좌 변량
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 
-counts = []
-# 강수량 올라가는건 그냥 반복호출해도 됨 
+def find_safety_zone(rain,x,y,visited):
+
+    for m in range(4):
+        nx = x + dx[m]
+        ny = y + dy[m]
+        if (0 <= nx < N) and (0 <= ny < N) and W[nx][ny] > rain and not visited[nx][ny]:
+            visited[nx][ny] = True
+            find_safety_zone(rain,nx,ny,visited)
+
+
+max_counts = []
 for rain in range(101):
     visited = [[False] * N for _ in range(N)]
     count = 0
     for i in range(N):
         for j in range(N):
             if W[i][j] > rain and not visited[i][j]:
-                count += 1
-                dfs(i,j,rain,visited)
-    counts.append(count)
-print(max(counts))
+                count+= 1
+                find_safety_zone(rain,i,j,visited)
+    max_counts.append(count)
+
+print(max(max_counts))
+
