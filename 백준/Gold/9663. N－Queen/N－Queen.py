d36839653908e92b29d5426a9ea982590d4101d9
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-16 17:49:06 +0900

    [Gold IV] Title: N-Queen, Time: 20308 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,26 +1,22 @@
 N = int(input())
-
-pos = [0] * N
-flag_a = [False] * N
-flag_b = [False] * (2 * N - 1)
-flag_c = [False] * (2 * N - 1)
-
+visited = [False] * N
+daegak_1 = [False] * (2 * N - 1)
+daegak_2 = [False] * (2 * N - 1)
 count = 0
 
-def set(i: int) -> None:
+def queen(i):
     global count
+    # base case
+    if i == N:
+        count += 1
+        return
+    
     for j in range(N):
-        if not flag_a[j] and not flag_b[i+j] and not flag_c[i-j+N-1]:
-            # print(f"진입: {i}열,{j}행에 퀸 놓기")
-            pos[i] = j
-            if i == N-1:
-                count += 1
-            else:
-                flag_a[j] = flag_b[i+j] = flag_c[i-j+N-1] = True
-                set(i + 1)
-                # print(f"<- 백트래킹: {i}열,{j}행 해제")
-                flag_a[j] = flag_b[i+j] = flag_c[i-j+N-1] = False
-                # print("flag:",flag)
-set(0)
+        if not visited[j] and not daegak_1[i+j] and not daegak_2[i-j+N-1]:
+            visited[j] = daegak_1[i+j] = daegak_2[i-j+N-1] = True
+            queen(i+1)           
+            visited[j] = daegak_1[i+j] = daegak_2[i-j+N-1] = False
 
+queen(0)
 print(count)
+            
