43c917576d266af3e52803f218665ecd9c3180ed
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-18 00:15:32 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,11 +1,21 @@
-from itertools import combinations
-
 N, S = map(int, input().split())
-nums = list(map(int, input().split()))
+num_list = list(map(int, input().split()))
+
+
+def combination(index, total):
+    global count
+    if index == N:
+        if total == S:
+            count += 1
+        return
+    combination(index + 1, total)
+    combination(index + 1, total + num_list[index])
+
 
 count = 0
-for i in range(1, N + 1):
-    for c in list(combinations(nums, i)):
-        if sum(c) == S:
-            count += 1
+combination(0, 0)
+
+if S == 0:
+    count -= 1
+
 print(count)
