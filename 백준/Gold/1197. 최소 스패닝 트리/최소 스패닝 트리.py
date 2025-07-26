38b07e705b941c63e298f834e58a6e9e8313ec28
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-27 00:53:02 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 364 ms, Memory: 151600 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,47 +1,52 @@
 import sys
-sys.setrecursionlimit(10**6)
+import heapq
+from collections import defaultdict
 
 input = sys.stdin.readline
 V, E = map(int, input().rstrip().split())
 
-
-# 크루스칼 알고리즘
 edges = []
 for _ in range(E):
     A, B, C = map(int, input().rstrip().split())
     edges.append((A, B, C))
 
-edges.sort(key=lambda x: x[2])
+# 프림 알고리즘
 
-# union-find
-parent = [i for i in range(V + 1)]
+# 그래프 선언
+graph = defaultdict(list)
 
+# 간선 정보
+# edges = [(1, 2, 3), (1, 3, 2), (2, 3, 1), (2, 4, 4), (3, 4, 5), (4, 5, 7), (3, 5, 6)]
 
-def get_parent(x):
-    if parent[x] == x:
-        return x
-    parent[x] = get_parent(parent[x])
-    return parent[x]
-
-
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
-
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+# 무방향 그래프 구성
+for u, v, w in edges:
+    graph[u].append((w, v))  # (가중치,목적지)
+    graph[v].append((w, u))  # 양방향 추가
 
 
-def same_parent(a, b):
-    return get_parent(a) == get_parent(b)
+# Prim 알고리즘 시작
+def prim(start):
+    visited = set()  # 방문한 정점
+    min_heap = []  # 우선순위 큐
+    total_cost = 0  # 최소 비용 합계
 
+    # 초기 노드에서 갈 수 있는 간선들 큐에 삽입
+    visited.add(start)
+    for weight, neighbor in graph[start]:
+        heapq.heappush(min_heap, (weight, neighbor))
 
-answer = 0
-for a, b, cost in edges:
-    if not same_parent(a, b):
-        union_parent(a, b)
-        answer += cost
+    while min_heap:
+        weight, node = heapq.heappop(min_heap)
+        if node in visited:
+            continue  # 이미 연결된 정점은 무시
 
-print(answer)
+        visited.add(node)
+        total_cost += weight
+
+        for next_weight, next_node in graph[node]:
+            if next_node not in visited:
+                heapq.heappush(min_heap, (next_weight, next_node))
+
+    return total_cost
+
+print(prim(1))
