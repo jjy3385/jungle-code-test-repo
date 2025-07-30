0b2f82ee7be9f42e8e454018a3f460eeadf2a3e9
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-30 23:16:09 +0900

    [Silver II] Title: DFS와 BFS, Time: 136 ms, Memory: 114628 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,58 +1,46 @@
-
 import sys
+
 from collections import defaultdict, deque
 
-# sys.stdin = open("input.txt", "r")
-
 input = sys.stdin.readline
-# 정점의 개수 N
-# 간선의 개수 M
-# 시작정점 V
+
+# 정점의 개수 N, 간선의 개수M, 시작 정점 V
 N, M, V = map(int, input().rstrip().split())
 
-# 1.그래프의 표현 - 인접리스트
 graph = defaultdict(list)
 for _ in range(M):
     a, b = map(int, input().rstrip().split())
     graph[a].append(b)
     graph[b].append(a)
 
-
-# 2.그래프 정렬 -  필요함
-# 다음 방문 정점이 결정되기 때문에 결과가 달라짐
+# 그래프 정렬
 for i in range(1, N + 1):
     graph[i].sort()
 
 
-# dfs - 재귀
-def dfs(v, visited):
-    # base case
-    visited[v] = True
-    print(v, end=" ")
-
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(i, visited)
+def dfs(start, visited):
+    visited[start] = True
+    print(start, end=" ")
+    for next_node in graph[start]:
+        if not visited[next_node]:
+            dfs(next_node,visited)
 
 
-# bfs - 큐
-def bfs(v):
+def bfs(start):
+    queue = deque([start])
     visited = [False] * (N + 1)
-
-    # 시작정점 방문처리
-    queue = deque([v])
-    visited[v] = True
-
+    visited[start] = True
     while queue:
-        v = queue.popleft()
-        print(v, end=" ")
-        for next_v in graph[v]:
-            if not visited[next_v]:
-                queue.append(next_v)
-                visited[next_v] = True
+        node = queue.popleft()
+        print(node, end=" ")
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+                
 
 
 visited = [False] * (N + 1)
-dfs(V, visited)
+dfs(V,visited)
 print()
 bfs(V)
