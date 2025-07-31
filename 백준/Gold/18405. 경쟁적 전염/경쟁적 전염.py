6d2d21aab809f174ad7abbd2be8376db6913ea69
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-07-31 15:57:42 +0900

    [Gold V] Title: 경쟁적 전염, Time: 208 ms, Memory: 115816 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -3,39 +3,46 @@ import heapq
 
 input = sys.stdin.readline
 
-n, k = tuple(map(int, input().split()))
-maps = []
+# N*N 크기의 시험관 ,K는 바이러스 종류
+N, K = map(int, input().rstrip().split())
 
-for _ in range(n):
-    maps.append(list(map(int, input().split())))
-s, y, x = tuple(map(int, input().split()))
+# 시험관 정보
+locations = [list(map(int, input().rstrip().split())) for _ in range(N)]
 
-dirs = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-# visited = [[False] * n for i in range(n)]
+# S초 지난 후에 (X,Y) 에 존재하는 바이러스의 종류를 출력하라
+S, X, Y = map(int, input().rstrip().split())
 
-pq = []
-for i in range(n):
-    for j in range(n):
-        if maps[i][j] > 0:
-            heapq.heappush(pq, (maps[i][j], i, j))
+def bfs():
+    global heap
+    new_heap = []
+    while heap:
+        virus, qx, qy = heapq.heappop(heap)
+        for i in range(4):
+            nx = qx + dx[i]
+            ny = qy + dy[i]
 
+            if ((0 <= nx < N) and (0 <= ny < N)) and locations[nx][ny] == 0:
 
-def spread():
-    global pq
+                locations[nx][ny] = virus
+                heapq.heappush(new_heap, (virus, nx, ny))
 
-    new_v = []
-    while pq:
-        kind, y, x = heapq.heappop(pq)
-        for dy, dx in dirs:
-            ny, nx = y + dy, x + dx
-            if ny < 0 or nx < 0 or ny >= n or nx >= n or maps[ny][nx] > 0:
-                continue
-            maps[ny][nx] = kind
-            heapq.heappush(new_v, (kind, ny, nx))
-    pq = new_v
+    heap = new_heap
 
 
-for _ in range(s):
-    spread()
+heap = []
 
-print(maps[y - 1][x - 1])
+# 상하좌우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for i in range(N):
+    for j in range(N):
+        if locations[i][j] != 0:
+            heapq.heappush(
+                heap, (locations[i][j], i, j)
+            )  # 우선순위 큐에 (바이러스,좌표) 입력
+
+for _ in range(S):
+    bfs()
+
+print(locations[X - 1][Y - 1])
