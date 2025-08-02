cafa8b896c9016323a95b9e17e2e95f7e6f5f4b0
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-02 17:09:20 +0900

    [Silver IV] Title: 동전 0, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -6,21 +6,12 @@ N, K = map(int, input().rstrip().split())
 # 동전의 종류
 coins = [int(input().rstrip()) for _ in range(N)]
 
-# K원을 만들기 위해 필요한 동전의 개수 최솟값
-remain = K
-
 count = 0
-while remain > 0:
+remain = K 
+for coin in sorted(coins, reverse=True):
+    if remain >= coin:
+        count += remain // coin
+        remain = remain % coin
 
-    i = 0
-    while (0 <= i < N) and coins[i] <= remain:
-        i += 1
-    i -= 1
-    
-    # print(remain, coins[i])
-    remain -= coins[i]
-    count += 1
-
-
-# print(remain)
 print(count)
+
