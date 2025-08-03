0c9323b4585839afc9a8380bd460508874c06847
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-03 21:56:12 +0900

    [Silver II] Title: 외판원 순회 2, Time: 184 ms, Memory: 134600 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,35 +1,43 @@
-N = int(input())
-W = [list(map(int,input().split())) for _ in range(N)]
+import sys
 
-visited = [False] * N
-path = []
-depth = 0
-min_costs = []
+input = sys.stdin.readline
 
-def dfs_last(depth,cost,current,start):
-    # base case
-    if depth == N:
+# 도시의 수
+N = int(input().rstrip())
+# W[i][j] = 도시i에서 도시 j로 가기 위한 비용
+W = [list(map(int, input().rstrip().split())) for _ in range(N)]
+
+# 우리가 구하는 것은 외판원이 순회에 필요한 최소비용이다
+
+# 유명한 외판원 순회문제
+# for i in range(N):
+#     for j in range(N):
+#         print(W[i][j], end=" ")
+#     print()
+
+
+def dfs(depth, current, start, cost):
+    if depth == N - 1:
+
         if W[current][start] != 0:
-            min_costs.append(cost + W[current][start])        
+            total_cost = cost + W[current][start]
+            # print(f"path = {path}, cost = {total_cost}")
+            costs.append(total_cost)
         return
 
     for i in range(N):
         if not visited[i] and W[current][i] != 0:
             visited[i] = True
             path.append(i)
-
             new_cost = cost + W[current][i]
-        
-            # 기준도시도 바뀐다
-            dfs_last(depth + 1,new_cost,i,start)
-            
-            path.pop()
+            dfs(depth + 1, i, start, new_cost)
             visited[i] = False
+            path.pop()
 
+
+visited = [False] * N
 visited[0] = True
-path.append(0)
-dfs_last(1,0,0,0)
-
-print(min(min_costs))
-
-
+path = []
+costs = []
+dfs(0, 0, 0, 0)
+print(min(costs))
