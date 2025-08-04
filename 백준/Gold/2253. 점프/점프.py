bd5d8fb57e9d53a5890dbed6ce9e6aa39e4d8d9a
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-04 14:17:02 +0900

    [Gold IV] Title: 점프, Time: 1436 ms, Memory: 221200 KB -BaekjoonHub

--- 백준/Gold/2253. 점프/점프.py
+++ 백준/Gold/2253. 점프/점프.py
@@ -1,41 +1,40 @@
 import sys
-
-sys.setrecursionlimit(10**4)
+from collections import deque
 
 input = sys.stdin.readline
 N, M = map(int, input().rstrip().split())
 stones = [int(input().rstrip()) for _ in range(M)]
 
-dp = {}
-dx = [-1, 0, 1]
-INF = int(1e9)
+
+def bfs(now, jump):
+    # visited = [[False] * (N + 1) for _ in range(N + 1)]  # visited[현재돌][점프] 
+    # 이거 메모리 초과남
+    visited = set()
+    visited.add((now, jump))
+    queue = deque([(now, jump, 0)])  # (현재돌,점프,점프횟수)
+
+    while queue:
+
+        prev_stone, prev_jump, prev_cnt = queue.popleft()
+        # print(prev_stone, prev_jump, prev_cnt)
+
+        if prev_stone == N:
+            return prev_cnt
+
+        for d in [-1, 0, 1]:
+            next_jump = prev_jump + d
+            next_stone = prev_stone + next_jump
+
+            if (
+                next_jump > 0
+                and next_stone <= N
+                and next_stone not in stones
+                and (next_stone,next_jump) not in visited
+            ):
+                queue.append((next_stone, next_jump, prev_cnt + 1))
+                visited.add((next_stone,next_jump))
+
+    return -1
 
 
-def dfs_dp(now, x):
-
-    #print(dp)
-    # base case
-    if now == N:
-        return 0
-
-    # 이미 지나갔던 경로인 경우
-    if (now, x) in dp:
-        return dp[(now, x)]
-
-    min_junp = INF
-    for i in range(3):
-
-        nx = x + dx[i]
-        nxt = now + nx
-
-        if nx > 0 and nxt <= N and nxt not in stones:
-            jump = dfs_dp(nxt, nx)
-            if jump != INF:
-                min_junp = min(min_junp, jump + 1)
-
-    dp[(now, x)] = min_junp
-    return min_junp
-
-
-ans = dfs_dp(1, 0)
-print(ans if ans != INF else -1)
+print(bfs(1, 0))
