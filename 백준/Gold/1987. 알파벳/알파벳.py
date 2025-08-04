b391709e648a8e33dfc8abd873b7e60730269bb6
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-04 16:41:26 +0900

    [Gold IV] Title: 알파벳, Time: 7060 ms, Memory: 160616 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,3 +1,4 @@
+
 import sys
 from collections import deque
 
@@ -5,10 +6,8 @@ input = sys.stdin.readline
 R, C = map(int, input().rstrip().split())
 board = [list(input().rstrip()) for _ in range(R)]
 
-visited = set()
-
 def dfs(r, c, cnt):
-
+    global max_cnt
     # 상하좌우
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
@@ -23,9 +22,11 @@ def dfs(r, c, cnt):
             visited.remove(board[nr][nc])
         else:
             # base case
-            res.add(cnt)
+            max_cnt = max(max_cnt,cnt)
 
-res = set() 
+# res = set() 
+max_cnt = 0
+visited = set()
 visited.add(board[0][0])
 dfs(0, 0, 1)
-print(max(res))
+print(max_cnt)
