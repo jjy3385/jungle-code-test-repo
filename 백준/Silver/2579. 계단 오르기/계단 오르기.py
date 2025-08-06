4faccdd3d855aefedaa7543f6b729cd755f6bede
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-06 23:11:04 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -5,27 +5,20 @@ input = sys.stdin.readline
 # 계단의 개수
 N = int(input().rstrip())
 # 각 계단에 쓰여있는 점수
-scores = [int(input().rstrip()) for _ in range(N)]
+scores = [0] + [int(input().rstrip()) for _ in range(N)]
 
 dp = [0] * (N + 1)
 
-# 이런게 뭐 점수가 있거나 무게가 있거나 이런 애들은 + scores[i] 이런게 붙음
-# 대충 dp[i] = dp[i] + scores[i] 이런 느낌으로
-# 이 문제에서 일단 dp[i] 는 i번째 계단을 밟았을 때 얻는 총점이니까 dp[i-1] 이나 dp[i-2] 이 둘의 총점 max + 이번 scores[i] 합친게 되겠네
+if N >= 1:
+    dp[1] = scores[1]
+if N >= 2:
+    dp[2] = scores[1] + scores[2]
+if N >= 3:
+    # 3번째 계단 밟았으니까 scores[3] 은 항상 포함되어야 함
+    # 그리고 이제 1번 밟았거나 2번 밟았거나 둘 중 큰 게 dp[3]
+    dp[3] = max(scores[1] + scores[3], scores[2] + scores[3])
 
-if N == 1:
-    print(scores[0])
-elif N == 2:
-    print(scores[0] + scores[1])
-elif N == 3:
-    print(max(scores[0] + scores[2], scores[1] + scores[2]))
-else:
-    dp[1] = scores[0]
-    dp[2] = scores[0] + scores[1]
-    dp[3] = max(scores[0] + scores[2], scores[1] + scores[2])
-
-    # print(dp[1], dp[2], dp[3])
-    for i in range(4, N + 1):
-        dp[i] = max(dp[i - 2], dp[i - 3] + scores[i - 2]) + scores[i - 1]
-
-    print(dp[N])
+for i in range(4, N + 1):
+    dp[i] = max(dp[i - 3] + scores[i - 1], dp[i - 2]) + scores[i]
+    
+print(dp[N])
