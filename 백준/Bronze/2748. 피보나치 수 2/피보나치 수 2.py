d1b1d6ae3016d0c0508220416a4876a749e9af9b
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-06 14:08:51 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -4,8 +4,13 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
+# 1.문제 목표: N번째 피보나치 수를 구하라
+# 2.상태 정의: dp[i]는 무엇인가? i번째 피보나치수의 값
+# 3.점화식? 주어짐, dp[n] = dp[n-1] + dp[n-2] (n>=2)
+# 4.초기값 dp[0] = 0 , dp[1] = 1
+# 5.정답위치는 dp[N]
+
 dp = [0] * (N + 1)
-dp[0] = 0
 dp[1] = 1
 
 for i in range(2, N + 1):
