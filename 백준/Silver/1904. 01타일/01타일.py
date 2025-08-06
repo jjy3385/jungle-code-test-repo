799a8518bd31375c66f37ce0356f193cb0ba2b44
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-06 15:19:40 +0900

    [Silver III] Title: 01타일, Time: 100 ms, Memory: 117360 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,18 +1,17 @@
+
 import sys
 
 input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-# 길이가 N인 만들 수 있는 2진 수열의 개수를 15746으로 나눈 나머지 출력
-
-dp = [0] * (N + 2)
-
+dp = [0] * (N + 1)
+dp[0] = 1
 dp[1] = 1
-dp[2] = 2
 
-for i in range(3, N + 1):
-    dp[i] = (dp[i - 1] % 15746) + (dp[i - 2] % 15746)
+for i in range(2, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
 
 
-print(dp[N] % 15746)
+# print(dp)
+print(dp[N])
