504c254537b9afc0176ceb9999fddb50100b628c
진주영 <67181810+jjy3385@users.noreply.github.com>
2025-08-07 00:40:06 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -4,19 +4,14 @@ input = sys.stdin.readline
 
 N = int(input().rstrip())
 
-# 그냥 dfs 로 풀릴거 같은데? 아닌듯
-# DP 정의해서 푸는 문제 같음
-# dp[i] 는 i를 1로 만드는 최소 연산횟수
 dp = [0] * (N + 1)
-dp[1] = 0
+dp[1] = 0 
 
 for i in range(2, N + 1):
+    dp[i] = min(
+        dp[i - 1] + 1,
+        dp[i // 2] + 1 if i % 2 == 0 else int(1e9),
+        dp[i // 3] + 1 if i % 3 == 0 else int(1e9),
+    )
 
-    dp[i] = dp[i - 1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
-
-
-print(dp[N])
+print(dp[-1])
