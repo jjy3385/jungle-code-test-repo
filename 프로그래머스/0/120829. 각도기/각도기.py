f938eb965443e4c0492135de9bc6cb90bade6a17
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-06-30 10:52:45 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.py
+++ 프로그래머스/0/120829. 각도기/각도기.py
@@ -1,12 +1,8 @@
 
 def solution(angle):
     answer = 0
-    if 0 < angle < 90:
-        answer = 1
-    elif angle == 90:
-        answer = 2
-    elif 90 < angle < 180:
-        answer = 3
-    elif angle == 180:
-        answer = 4
+    if angle <= 90:
+        answer = 1 if angle < 90 else 2
+    else:
+        answer = 3 if angle < 180 else 4
     return answer
