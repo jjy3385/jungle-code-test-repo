75c1775695338387d600502d3f2ce0a3d396a64b
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-10 12:11:39 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,3 +1,3 @@
 def solution(cipher, code):
-    answer = ''.join(v for i,v in enumerate(cipher,1) if i % code == 0)
-    return answer
+    # answer = ''.join(v for i,v in enumerate(cipher,1) if i % code == 0)
+    return cipher[code-1::code]
