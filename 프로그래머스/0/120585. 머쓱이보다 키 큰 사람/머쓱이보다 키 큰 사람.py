ba8eb36280c3ac38edc7571df4a198906f8ebcd0
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-16 11:00:58 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,8 +1,5 @@
 def solution(array, height):
-    answer = 0
     array.append(height)
-    for i,v in enumerate(sorted(array, reverse=True)):
-        if height == v:
-            answer = i
-            break
-    return answer
+    array.sort(reverse=True)
+    return array.index(height)
+    
