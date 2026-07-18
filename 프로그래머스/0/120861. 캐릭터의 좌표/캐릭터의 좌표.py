e3c864ebcc9ea0d90dc95f903cbb127fe1ecfd11
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-18 17:26:00 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,19 @@
+def solution(keyinput, board):
+    x,y = 0,0
+    for k in keyinput:
+        dx,dy = 0,0
+        if k == "left":
+            dx = -1
+        elif k == "right":
+            dx = 1
+        elif k == "down":
+            dy = -1
+        else:
+            dy = +1
+        
+        if abs(x + dx) <= board[0] // 2:
+            x = x + dx
+        if abs(y + dy) <= board[1] // 2:
+            y = y + dy
+
+    return [x,y]
