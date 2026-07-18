48da2459e1a626b9264ba490d3e3d65974993aaf
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-18 19:06:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,6 +1,4 @@
 def solution(numbers):
     numbers.sort()
-    idx = len(numbers) - 1
-    max1 = numbers[0] * numbers[1]
-    max2 = numbers[idx] * numbers[idx - 1]
-    return max(max1,max2)
+
+    return max(numbers[0] * numbers[1],numbers[-1] * numbers[-2])
