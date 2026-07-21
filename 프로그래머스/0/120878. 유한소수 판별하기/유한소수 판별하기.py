e0a6ac850357a6500fd7d0a2b3d773af833e98da
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-21 13:49:51 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -1,17 +1,16 @@
 def solution(a, b):
-    answer = 2
     b = b // gcd(a,b)
     
     while b % 2 == 0:
         b = b // 2
     while b % 5 == 0:
         b = b // 5
-    
+
     if b == 1:
-        answer = 1
-    return answer
+        return 1
+    return 2
 
 def gcd(a,b):
-    while b != 0:
+    while b:
         a, b = b, a % b
     return a
