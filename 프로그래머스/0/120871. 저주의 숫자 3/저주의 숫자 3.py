42eeb7709eeaeb1f9b78e8e19e76c7dbf743b46d
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-21 12:24:39 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = 0
-    for i in range(1,n + 1):
+    for _ in range(n):
         answer += 1
         while answer % 3 == 0 or '3' in str(answer):
             answer += 1           
