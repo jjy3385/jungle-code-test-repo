38cc1add6d36e7e095f6ccbd5f67f6a8b9ea2d73
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-21 12:57:17 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,10 +1,10 @@
 def solution(dots):
     answer = 0
-    if isPallel(dots,0,1,2,3) or isPallel(dots,0,2,1,3) or isPallel(dots,0,3,1,2):
+    if isParallel(dots,0,1,2,3) or isParallel(dots,0,2,1,3) or isParallel(dots,0,3,1,2):
         answer = 1
     return answer
 
-def isPallel(dots,a,b,c,d):
+def isParallel(dots,a,b,c,d):
     dy1 = dots[a][1] - dots[b][1]
     dx1 = dots[a][0] - dots[b][0]
     dy2 = dots[c][1] - dots[d][1]
