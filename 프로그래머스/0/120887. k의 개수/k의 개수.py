468c1eb98da82e2806bccc52eaebbf1489006a65
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-24 15:28:50 +0900

    [level 0] Title: k의 개수, Time: 9.26 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,7 +1,5 @@
 def solution(i, j, k):
     answer = 0
-    sk = str(k)
     for x in range(i,j + 1):        
-        if sk in str(x):
-            answer += str(x).count(sk)
+        answer += str(x).count(str(k))
     return answer
