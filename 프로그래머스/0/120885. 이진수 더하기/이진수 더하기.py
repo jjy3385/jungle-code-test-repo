84ef47b0789fc8ccd09a243eb3166f318406cd68
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-24 15:51:41 +0900

    [level 0] Title: 이진수 더하기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,33 +1,15 @@
 def solution(bin1, bin2):
     answer = ''
-    i = len(bin1) - 1
-    j = len(bin2) - 1
+    length = max(len(bin1),len(bin2))
+    bin1,bin2 = bin1.zfill(length),bin2.zfill(length)
     carry = 0
-    curr = 0
-    while i >= 0 or j >= 0 or carry:
-        bin1_temp = 0
-        bin2_temp = 0        
-        if i >= 0 and bin1[i] == '1':
-            bin1_temp = 1
-        if j >= 0 and bin2[j] == '1':
-            bin2_temp = 1
-            
-        if bin1_temp + bin2_temp + carry == 3:
-            carry = 1
-            curr = 1
-        elif bin1_temp + bin2_temp + carry == 2:
-            carry = 1
-            curr  = 0
-        elif bin1_temp + bin2_temp + carry == 1:
-            carry = 0
-            curr  = 1
-        else:
-            carry = 0
-            curr  = 0
-            
-        answer = str(curr) + answer   
-            
-        i -= 1
-        j -= 1
-        
-    return answer
+    for x,y in zip(bin1[::-1],bin2[::-1]):
+        total = int(x) + int(y) + carry
+        curr = total % 2
+        carry = total // 2
+        answer += str(curr)
+    
+    if carry:
+        answer += str(carry)
+    
+    return answer[::-1]
