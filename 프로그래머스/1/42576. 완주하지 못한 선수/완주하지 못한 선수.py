82316840913c32d7eeaefba9d948f3c62f8d1ad9
진주영 <67181810+jjy3385@users.noreply.github.com>
2026-07-28 10:45:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 72.76 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,5 @@
+import collections
 def solution(participant, completion):
-    count = {}
-    for p in participant:
-        count[p] = count.get(p,0) + 1
-    
-    for name in completion:
-        count[name] -= 1
-    
-    for n,c in count.items():
-        if c == 1:
-            return n
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    answer = next(iter(answer))
+    return answer
